406849c03628575671919e9ac127454a1db5c1ff
Jonas Mosbech <jonas@mosbech.net>
2017-01-05 12:54:07 +0100

    Add comments

--- soap/node/create-cash-book-entry.js
+++ soap/node/create-cash-book-entry.js
@@ -25,13 +25,20 @@ soap.createClient(url, (err, client) => {
 
 		var cashBookEntryData = {
 			data: {
+				// DebtorPayment, CreditorInvoice, CreditorPayment, FinanceVoucher or ManualDebtorInvoice
 				Type: "FinanceVoucher",
+				// in which CashBook do you want to create the entry
 				CashBookHandle: {Number: CASH_BOOK_ID},
+				// on which account do you want to book the entry
 				AccountHandle: {Number: 1000},
+				// which contra account should be used in the entry
 				ContraAccountHandle: {Number: 1100},
 				Date: "2017-01-04T00:00:00.000Z",
+				// when creating multi line transacations you can reuse the VoucherNumber 
+				// to indicate that they are originating from the same document
 				VoucherNumber: 42,
 				Text: "Bank deposit",
+				// when creating an entry in the default currency this should be the same as the Amount
 				AmountDefaultCurrency: 1337.00,
 				CurrencyHandle: {Code: "EUR"},
 				Amount: 1337.00
